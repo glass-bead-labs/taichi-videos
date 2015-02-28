1bb9fcec1b8a25ff12db2b7fb86ecce28c9aaba1
Dav Clark <davclark@berkeley.edu>
2015-02-27 20:02:23 -0800

    Trying to get local files working... :(

--- taichi_videos/main.py
+++ taichi_videos/main.py
@@ -1,3 +1,5 @@
+import os
+
 import morepath
 from more import static
 import bowerstatic
@@ -11,9 +13,20 @@ class App(static.StaticApp):
 ## Basic Bower stuff
 
 bower = bowerstatic.Bower()
-# Currently configuring to look from directory where run
-components = bower.components('app', 
-                        '/home/dav/Projects/taichi-videos/bower_components')
+# Currently configured to look from directory where run
+curr_dir = os.getcwd()
+components = bower.components('app',
+                              os.path.join(curr_dir, 'bower_components') )
+
+### This stuff is just straight not working... :(
+
+# # Local components must still have a bower.json
+# local = bower.local_components('local', components)
+# local.component(os.path.join(curr_dir, 'resources/taichi_style'),
+#                 # Make the "version" change whenever code is changed
+#                 # This should be changed for "production", but we're unlikely to
+#                 # use this code in high-volume situations.
+#                 version=None)
 
 @App.static_components()
 def get_static_components():
@@ -21,37 +34,33 @@ def get_static_components():
 
 ## Site root
 
-@App.path(path='static')
-class Root(object):
-        pass
-
 @App.path(path='')
 class Root(object):
     pass
 
-#@App.static_components()
-#def get_static_components():
-#    return components
-
 @App.html(model=Root)
 def hello_word(self, request):
     request.include('bootstrap')
-    # request.include('jquery/dist/jquery.min.js')
+    # We could further simplify by making bootstrap a bower dependency
+    # Not working for some reason
+    # request.include('taichi_style')
     with open('resources/index.html', 'r') as file_obj:
         result = file_obj.read()
     return result
 
-class Static(object):
-    def __init__(self, path):
-        self.path = path
+# Example of how to get static paths... but doesn't work with nested dirs...
 
-@App.path(model=Static, path='/static/{path}')
-def path_name(path):
-    return Static(path)
-
-@App.view(model=Static)
-def serve_static(self, request):
-    return self.path
+# class Static(object):
+#     def __init__(self, path):
+#         self.path = path
+#
+# @App.path(model=Static, path='/static/{path}')
+# def path_name(path):
+#     return Static(path)
+#
+# @App.view(model=Static)
+# def serve_static(self, request):
+#     return self.path
 
 def main():
     config=morepath.setup()
