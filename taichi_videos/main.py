af8adb6b79f4b75b518fe24e7310477aab1a63bd
Dav Clark <davclark@berkeley.edu>
2015-04-14 13:51:21 -0700

    Fixed video/{id} views (and root)

--- taichi_videos/main.py
+++ taichi_videos/main.py
@@ -7,12 +7,13 @@ import morepath
 # from morepath.security import Identity, NO_IDENTITY
 from more.itsdangerous import IdentityPolicy
 
+from werkzeug.serving import run_simple
+
 from .app import App
 # Only for side-effects (noqa disables linter warning)
 from . import static  # noqa
-import Cookie
 
-#Templating
+# Templating
 #from more.jinja2 import Environment, PackageLoader, Jinja2App
 
 
@@ -34,23 +35,28 @@ def verify_identity(identity):
     # If we get this far, it means we've assigned a valid identity
     return True
 
+@App.permission_rule(model=object, permission=ViewPermission, identity=None)
+def has_permission_not_logged_in(identity, model, permission):
+    return True
+
 # Site root
 
 
-@App.path(path='')
+@App.path(path='/')
 class Root(object):
     pass
 
 
-@App.html(model=Root, permission=ViewPermission, name='root')
+@App.html(model=Root, permission=ViewPermission)
 def hello_word(self, request):
     # if not logged in, want to:
-    return morepath.redirect(request.link(login))
+    # return morepath.redirect(request.link(login))
     request.include('bootstrap')
     request.include('taichi_style')
 
     with open('resources/index.html', 'r') as file_obj:
         result = file_obj.read()
+        print(result)
 
     # No idea why I was having trouble with this earlier
     logging.info('I got a request at {}'.format(date.today()))
@@ -96,6 +102,7 @@ def login_form(self, request):
 class Signup(object):
     pass
 
+
 @App.html(model=Signup)
 def signup_form(self, request):
     request.include('bootstrap')
@@ -103,14 +110,16 @@ def signup_form(self, request):
         result = file_obj.read()
     return result
 
+
 @App.template_directory()
 def get_template_directory():
     return 'templates'
 
+
 def generate_template_key_value(src):
     with open(src) as csvfile:
         reader = csv.reader(csvfile)
-        mydict = {row[0]:row[1] for row in reader}
+        mydict = {row[0]: row[1] for row in reader}
     return mydict
 
 # {
@@ -121,58 +130,70 @@ def generate_template_key_value(src):
 # }
 # env = Environment(loader=PackageLoader('taichi_videos', 'templates'))
 # template = env.get_template('index.html')
-@App.html(model=Login, request_method='POST', template = "index.jinja2")
+
+
+@App.html(model=Login, request_method='POST', template="index.jinja2")
 def login_validate(self, request):
     username = request.POST['username']
     password = request.POST['password']
     if not self.user_has_password(username, password):
         return 'Sorry, invalid username/password combination'
-    #C = Cookie.SimpleCookie()
+
     @request.after
     def remember(response):
         identity = morepath.Identity(username)
         morepath.remember_identity(response, request, identity)
-    
+
     request.include('bootstrap')
     request.include('taichi_style')
 
     with open('resources/all-videos.csv') as csvfile:
         reader = csv.reader(csvfile)
         myrow = [row for row in reader][1:]
-    mydict = {'id':myrow[13][0], 'title':myrow[13][1], 'src':myrow[13][2], 'desc':myrow[13][3]}
-    template_values = mydict #generate_template_key_value('resources/template-key-value.csv')
+    mydict = {
+        'id': myrow[13][0],
+        'title': myrow[13][1],
+        'src': myrow[13][2],
+        'desc': myrow[13][3]}
+    # generate_template_key_value('resources/template-key-value.csv')
+    template_values = mydict
     # with open('templates/index.html', 'r') as file_obj:
     #     result = file_obj.read()
 
     # No idea why I was having trouble with this earlier
     #logging.info('I got a request at {}'.format(date.today()))
-    #template.render(template_values)
+    # template.render(template_values)
     return template_values
 
-    #return 
+    # return
     #C[str(username)] = str(password)
-    #return #'You typed {}, {}'.format(username, password)
+    # return #'You typed {}, {}'.format(username, password)
+
 
 class Video(object):
-    def __init__(self, id, title, src, desc):
-        self.id = id
-        self.title = title
-        self.src = src
-        self.desc = desc
 
-@App.path(model=Video,path='/video/{vidId}')
-def videoId(self):
-    with open('all-videos.csv') as csvfile:
-        reader = DictReader(csvfile)
-        rows = {row['id']:row for row in reader}
-    vid = Video(vidId, rows[vidId]['title'], rows[vidId]['src'], rows[vidId]['desc'])
-    return vid
+    def __init__(self, attribute_dict):
+        self.attribute_dict = attribute_dict
+
+with open('resources/all-videos.csv') as csvfile:
+    reader = DictReader(csvfile)
+    videos = {row['id']: Video(row) for row in reader}
+
+
+@App.path(model=Video, path='/video/{vidId}')
+def get_video(vidId):
+    return videos[vidId]
+
 
 @App.html(model=Video, template="index.jinja2")
-def video(self):
-    return rows[self.id]
+def video(self, request):
+    request.include('bootstrap')
+    request.include('taichi_style')
+    return self.attribute_dict
+
+
 def main():
-    logging.basicConfig(filename='log/events.csv', level=logging.INFO)
+    # logging.basicConfig(filename='log/events.csv', level=logging.INFO)
 
     # Now configuring to use autosetup
     # config=morepath.setup()
@@ -181,4 +202,5 @@ def main():
     # config.commit()
     morepath.autosetup()
     wsgi = App()
-    morepath.run(wsgi)
+    # morepath.run(wsgi)
+    run_simple('localhost', 5000, wsgi, use_reloader=True)
